628bcca9b213a9c628d838d68e20f715e2db76d4
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-03-01 22:07:43 +0900

    Add files via upload

---
+++ JavaScript_practiceII_object_an array whose elements are objects.js
@@ -0,0 +1,12 @@
+const characters = [
+    {name: "◆◆◆ ◆◆◆", age: 14},
+    {name: "*** ***", age: 1000}
+  ];
+  
+  // Please print the first element of the array "characters".
+  console.log(characters[0]);
+  
+  // Please print the value corresponding to "name" in the second element of the array "characters".
+  console.log(characters[1].name);
+  
+  
